b60936901dd10959d4dc5d14e7b386316129f3f5
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-04-20 05:35:00 +0530

    static file handler

--- net.js
+++ net.js
@@ -1,5 +1,8 @@
 const net = require('net')
+const fs = require('fs')
+const path = require('path')
 const routes = {'GET': {}, 'POST': {}}
+
 
 function createWebServer (requestHandler) {
   const server = net.createServer()
@@ -148,6 +151,53 @@ const addRoutes = (method, path, callback) => {
   routes[method][path] = callback
 }
 
+function staticFileHandler (directory) {
+  var filePath = '.' + request.url
+  if (filePath === './') { filePath = './index.html' }
+
+  var extname = path.extname(filePath)
+  var contentType = 'text/html'
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript'
+      break
+    case '.css':
+      contentType = 'text/css'
+      break
+    case '.json':
+      contentType = 'application/json'
+      break
+    case '.png':
+      contentType = 'image/png'
+      break
+    case '.jpg':
+      contentType = 'image/jpg'
+      break
+    case '.wav':
+      contentType = 'audio/wav'
+      break
+  }
+
+  fs.readFile(filePath, function (error, content) {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        fs.readFile('./404.html', function (error, content) {
+          response.writeHead(200, { 'Content-Type': contentType })
+          response.end(content, 'utf-8')
+        })
+      } else {
+        response.writeHead(500)
+        response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n')
+        response.end()
+      }
+    } else {
+      response.writeHead(200, { 'Content-Type': contentType })
+      response.end(content, 'utf-8')
+    }
+  })
+}
+
+
 const webServer = createWebServer((req, res) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`)
   addRoutes('GET', '/', (req, res) => {
@@ -175,3 +225,8 @@ const webServer = createWebServer((req, res) => {
 })
 
 webServer.listen(3000)
+webServer.addHandler((req, res, next) => {
+  next(req, res)
+})
+webServer.addHandler(staticFileHandler('./test'))
+
